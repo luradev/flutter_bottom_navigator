9cba6324ebb22847d7a4200b7970c8a4772319b9
Lucau Rafael <72351063+luranet@users.noreply.github.com>
2022-01-12 15:24:44 +0100

    A sample using BottomNavigationBar

---
+++ lib/card1.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'fooderlich_theme.dart';
+
+class Card1 extends StatelessWidget {
+  const Card1({Key? key}) : super(key: key);
+
+  final String category = 'Editor\'s Choice';
+  final String title = 'The Art of Dough';
+  final String description = 'Learn to make the perfect bread.';
+  final String chef = 'Ray Wenderlich';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Stack(
+          children: [
+            Text(
+              category,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+            Positioned(
+              child: Text(
+                title,
+                style: FooderlichTheme.darkTextTheme.headline5,
+              ),
+              top: 20,
+            ),
+            Positioned(
+              child: Text(
+                description,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 30,
+              right: 0,
+            ),
+            Positioned(
+              child: Text(
+                chef,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 10,
+              right: 0,
+            )
+          ],
+        ),
+        padding: const EdgeInsets.all(16),
+        constraints: const BoxConstraints.expand(height: 450, width: 350),
+        decoration: BoxDecoration(
+          image: const DecorationImage(
+            image: NetworkImage(
+                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import 'card1.dart';
+import 'fooderlich_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final tema = FooderlichTheme.dark();
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: tema,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, String? title}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static var paginas = [
+    const Card1(),
+    Container(
+      color: Colors.green,
+    ),
+    Container(
+      color: Colors.red,
+    )
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Bottom Navigation',
+          style: FooderlichTheme.dark().textTheme.headline6,
+        ),
+        centerTitle: true,
+      ),
+      body: paginas[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.orange,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card1'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card2'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.card_giftcard), label: 'Card3')
+          ]),
+    );
+  }
+}
